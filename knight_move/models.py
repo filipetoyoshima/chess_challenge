7abfe9f473eb782108e4f087808c80f2d7dfdc12
Filipe Toyoshima <filipetoyoshima@gmail.com>
2022-02-24 18:44:15 -0300

    creating piece model

--- knight_move/models.py
+++ knight_move/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Piece(models.Model):
+    type = models.CharField(max_length=1, choices=([
+        ('k', 'King'),
+        ('q', 'Queen'),
+        ('r', 'Rook'),
+        ('b', 'Bishop'),
+        ('n', 'Knight'),
+        ('p', 'Pawn'),
+    ]))
+    color = models.BooleanField(choices=([
+        (True, 'White'),
+        (False, 'Black'),
+    ]))
+    x_coord = models.IntegerField()
+    y_coord = models.IntegerField()
+
+    def __str__(self):
+        color = 'White' if self.color else 'Black'
+        columnName = chr(self.x_coord + ord('A'))
+        return f'{color}_{self.type}{columnName}{self.y_coord + 1}'
