3671a0163cebacffca8188d774bf688f427c9a25
Filipe Toyoshima <filipetoyoshima@gmail.com>
2022-02-24 20:24:45 -0300

    getting board settings

--- knight_move/urls.py
+++ knight_move/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('register_piece/', views.register_piece, name='register_piece'),
+    path('get_board/', views.get_board, name='get_board'),
+    path('get_board/<str:type>/', views.get_board, name='get_board'),
 ]
--- knight_move/views.py
+++ knight_move/views.py
@@ -16,3 +16,19 @@ def register_piece(request):
         return HttpResponse(status=409, content='Piece already exists at this location')
     piece = Piece.objects.create(**body)
     return HttpResponse(status=201, content=json.dumps(piece.__repr__()))
+
+
+def get_board(request, type='string'):
+    if request.method != 'GET':
+        return HttpResponse(status=405)
+    pieces = Piece.objects.all()
+    matrix = [[f'{x}{y}' for x in range(8)] for y in range(8)]
+    for piece in pieces:
+        matrix[piece.x_coord][7 - piece.y_coord] = f'{"w" if piece.color else "b"}{piece.type}'
+    
+    if type == 'string':
+        return HttpResponse(status=200, content=f'\n'.join([' '.join(row) for row in matrix]))
+    elif type == 'json':
+        return HttpResponse(status=200, content=json.dumps(matrix))
+    else:
+        return HttpResponse(status=400, content='Invalid type')
