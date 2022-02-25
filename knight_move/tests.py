cfa90d9c1b1203fc68730bdfbb7ac51eff55bf90
Filipe Toyoshima <filipetoyoshima@gmail.com>
2022-02-25 09:59:56 -0300

    testing board visualization

--- knight_move/tests.py
+++ knight_move/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from .models import Piece
 
-# Create your tests here.
+BOARD = """__ __ __ __ __ __ __ __
+__ __ __ __ __ __ __ __
+__ __ __ __ __ __ __ __
+__ __ __ __ __ __ __ __
+__ __ __ __ __ __ __ __
+__ __ __ __ __ __ __ __
+__ __ __ __ __ __ __ __
+wN __ __ __ __ __ __ __"""
+
+
+class KnightMoveTestCase(TestCase):
+    def setUp(self):
+        Piece.objects.create(**{
+            'type': 'n',
+            'color': True,
+            'x_coord': 0,
+            'y_coord': 0,
+        })
+
+    def test_get_board(self):
+        self.maxDiff = None
+        response = self.client.get('/knight_move/get_board/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            response.content.decode('utf-8'),
+            BOARD
+        )
